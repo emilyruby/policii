477370128cad83cb2348378d0cee915b80152f71
emilyruby <zcabeme@ucl.ac.uk>
2017-06-14 14:35:51 +0100

    got markdown rendering to work

--- policii/src/components/Parties/Conservative.vue
+++ policii/src/components/Parties/Conservative.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
-    Good Morning America!
+  <div v-html="content">
   </div>
 </template>
 
 <script>
   export default {
     name: 'Conservative',
+    data: function () {
+      return {
+        html: ''
+      }
+    },
     props: [
       'heading'
-    ]
+    ],
+    computed: {
+      content: function () {
+        const showdown = require('showdown')
+        const converter = new showdown.Converter()
+        const text = '#hello, markdown!'
+        const html = converter.makeHtml(text)
+        return html
+      }
+    }
   }
 </script>
 
