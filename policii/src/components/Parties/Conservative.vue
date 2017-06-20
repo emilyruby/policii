a4516e93ea4659031acaf9af58cf417f066db2b4
emilyruby <zcabeme@ucl.ac.uk>
2017-06-20 11:32:42 +0100

    updated component to have child components and pass down props

--- policii/src/components/Parties/Conservative.vue
+++ policii/src/components/Parties/Conservative.vue
@@ -1,22 +1,71 @@
 <template>
-  <div id="policies" v-html="content">
+  <div id="policies" v-html="html">
+    {{title}}
+    <brexit v-show="title === 'brexit'" :heading="heading">
+      {{title}}
+    </brexit>
+    <disabilities v-show="title"></disabilities>
+    <diverse v-show="title"></diverse>
+    <economy v-show="title"></economy>
+    <education v-show="title"></education>
+    <environment v-show="title"></environment>
+    <foreign v-show="title"></foreign>
+    <healthcare v-show="title"></healthcare>
+    <housing v-show="title"></housing>
+    <industry v-show="title"></industry>
+    <infrastructure v-show="title"></infrastructure>
+    <local v-show="title"></local>
+    <security v-show="title"></security>
+    <social v-show="title"></social>
   </div>
 </template>
 
 <script>
+  import Brexit from '@/components/Policies/Conservative/Brexit.vue'
+  import Disabilities from '@/components/Policies/Conservative/Disabilities.vue'
+  import Diverse from '@/components/Policies/Conservative/Diverse.vue'
+  import Economy from '@/components/Policies/Conservative/Economy.vue'
+  import Education from '@/components/Policies/Conservative/Education.vue'
+  import Environment from '@/components/Policies/Conservative/Environment.vue'
+  import Foreign from '@/components/Policies/Conservative/Foreign.vue'
+  import Healthcare from '@/components/Policies/Conservative/Healthcare.vue'
+  import Housing from '@/components/Policies/Conservative/Housing.vue'
+  import Industry from '@/components/Policies/Conservative/Industry.vue'
+  import Infrastructure from '@/components/Policies/Conservative/Infrastructure.vue'
+  import Local from '@/components/Policies/Conservative/Local.vue'
+  import Security from '@/components/Policies/Conservative/Security.vue'
+  import Social from '@/components/Policies/Conservative/Social.vue'
+
   export default {
     name: 'Conservative',
+    components: {
+      Brexit,
+      Disabilities,
+      Diverse,
+      Economy,
+      Education,
+      Environment,
+      Foreign,
+      Healthcare,
+      Housing,
+      Industry,
+      Infrastructure,
+      Local,
+      Security,
+      Social
+    },
     data: function () {
       return {
         html: ''
       }
     },
     props: [
-      'heading'
+      'heading',
+      'html'
     ],
     computed: {
-      content: function () {
-
+      title: function () {
+        return this.heading.split(' ')[0]
       }
     }
   }
