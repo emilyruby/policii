62522b2922287f99c38a1de1a57786635c63faff
emilyruby <zcabeme@ucl.ac.uk>
2017-06-20 11:32:13 +0100

    deleted html files and created vue components for each policy category

---
+++ policii/src/components/Policies/Conservative/Environment.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul>
+    <li>Want almost every car and van to be zero-emission by 2050- invest £600 million
+      by 2020 to help achieve it.</li>
+
+    <li>Invest more in low-emission buses, audio-visual displays for bus passengers,
+      and community minibuses for rural areas poorly served by public transport.</li>
+
+    <li>Devise a new agri-environment system.</li>
+
+    <li>Help <code>Natural England</code> to deliver environmental improvements.</li>
+
+    <li>Grant a free vote, on a government bill in government time, to give parliament
+      the opportunity to decide the future of the <code>Hunting Act</code>.</li>
+
+    <li>Produce a 25 year environment plan that will chart how we improve the environment.</li>
+
+    <li>Continue to support natural resource extractions from the <code>North Sea</code> and support
+      the oil and gas sector.</li>
+
+    <li>Commission an independent review into the cost of energy.</li>
+
+    <li>Support the development of wind projects in the remote islands of Scotland.</li>
+
+    <li>Legislate to change planning law for Shale applications.</li>
+
+    <li>Set up a new <code>Shale Environmental Regulator</code>.</li>
+
+    <li>Ensure smart meters are offered to every household and business by the end of
+      2020.</li>
+
+    <li>Introduce a safeguard tariff cap on energy bills for vulnerable customers.</li>
+
+    <li>Commit to upgrade all fuel poor homes to <code>EPC Band C</code> by 2030.</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'Environment',
+    props: [
+      'heading'
+    ]
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ policii/src/components/Policies/Conservative/Infrastructure.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul>
+    <li>Create extra capacity on our railways, which will ease overcrowding. Bring new
+      lines and stations, and improve existing routes- including for freight.</li>
+
+    <li>Help shipyards modernise and collaborate.</li>
+
+    <li>Continue investment into strategies such as <code>High Speed 2</code>, <code>Northern Powerhouse
+      Rail</code> and the expansion of Heathrow Airport.  </li>
+
+    <li>Target <code>National Productivity Investment Fund</code> spending at housing, research
+      and development, economic infrastructure and skills.</li>
+
+    <li>Review rail-ticketing and introduce a <code>Passenger Ombudsman</code>.</li>
+
+    <li>Ensure through the <code>Universal Service Obligation</code>, that every home and business
+      in Britain has access to high speed broadband. By the end of 2017, 19 out of
+      20 premises will have access to super-fast broadband.</li>
+
+    <li>By 2022, extend mobile coverage further to 95% geographic coverage of the UK.</li>
+
+    <li>By 2022, ensure all major roads and mainline trains have uninterrupted phone
+      service and guaranteed WiFi internet on all such trains.</li>
+
+    <li>Have a majority of the UK population covered by a 5G signal by 2027.</li>
+
+    <li>Roll out <code>Verify</code> so that people can identify themselves on all government
+      services by 2020.</li>
+
+    <li>Reduce data duplication across all systems, so that we automatically comply
+      with the <code>Once Only</code> principle in central government services by 2022 and
+      wider public services by 2025.</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'Infrastructure',
+    props: [
+      'heading'
+    ]
+  }
+</script>
+
+<style scoped>
+
+</style>
